bc47f4569f6b1516eb2ed9cce64ef1d467a23611
Joonyoung Roh <joonyoungr@gmail.com>
2019-07-07 16:10:48 +0900

    Chap3.6: insert Points (kd-tree)

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -25,7 +25,7 @@ pcl::visualization::PCLVisualizer::Ptr initScene(Box window, int zoom)
 pcl::PointCloud<pcl::PointXYZ>::Ptr CreateData(std::vector<std::vector<float>> points)
 {
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>());
-  	
+
   	for(int i = 0; i < points.size(); i++)
   	{
   		pcl::PointXYZ point;
@@ -81,7 +81,7 @@ std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<flo
 	// TODO: Fill out this function to return list of indices for each cluster
 
 	std::vector<std::vector<int>> clusters;
- 
+
 	return clusters;
 
 }
@@ -91,12 +91,12 @@ int main ()
 
 	// Create viewer
 	Box window;
-  	window.x_min = -10;
-  	window.x_max =  10;
-  	window.y_min = -10;
-  	window.y_max =  10;
-  	window.z_min =   0;
-  	window.z_max =   0;
+	window.x_min = -10;
+	window.x_max =  10;
+	window.y_min = -10;
+	window.y_max =  10;
+	window.z_min =   0;
+	window.z_max =   0;
 	pcl::visualization::PCLVisualizer::Ptr viewer = initScene(window, 25);
 
 	// Create data
@@ -105,30 +105,30 @@ int main ()
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = CreateData(points);
 
 	KdTree* tree = new KdTree;
-  
-    for (int i=0; i<points.size(); i++) 
-    	tree->insert(points[i],i); 
 
-  	int it = 0;
-  	render2DTree(tree->root,viewer,window, it);
-  
-  	std::cout << "Test Search" << std::endl;
-  	std::vector<int> nearby = tree->search({-6,7},3.0);
-  	for(int index : nearby)
-      std::cout << index << ",";
-  	std::cout << std::endl;
+  for (int i=0; i<points.size(); i++)
+  	tree->insert(points[i],i);
 
-  	// Time segmentation process
-  	auto startTime = std::chrono::steady_clock::now();
-  	//
-  	std::vector<std::vector<int>> clusters = euclideanCluster(points, tree, 3.0);
-  	//
-  	auto endTime = std::chrono::steady_clock::now();
-  	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
-  	std::cout << "clustering found " << clusters.size() << " and took " << elapsedTime.count() << " milliseconds" << std::endl;
+	int it = 0;
+	render2DTree(tree->root,viewer,window, it);
 
-  	// Render clusters
-  	int clusterId = 0;
+	std::cout << "Test Search" << std::endl;
+	std::vector<int> nearby = tree->search({-6,7},3.0);
+	for(int index : nearby)
+    std::cout << index << ",";
+	std::cout << std::endl;
+
+	// Time segmentation process
+	auto startTime = std::chrono::steady_clock::now();
+	//
+	std::vector<std::vector<int>> clusters = euclideanCluster(points, tree, 3.0);
+	//
+	auto endTime = std::chrono::steady_clock::now();
+	auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+	std::cout << "clustering found " << clusters.size() << " and took " << elapsedTime.count() << " milliseconds" << std::endl;
+
+	// Render clusters
+	int clusterId = 0;
 	std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1)};
   	for(std::vector<int> cluster : clusters)
   	{
@@ -140,10 +140,10 @@ int main ()
   	}
   	if(clusters.size()==0)
   		renderPointCloud(viewer,cloud,"data");
-	
+
   	while (!viewer->wasStopped ())
   	{
   	  viewer->spinOnce ();
   	}
-  	
+
 }
